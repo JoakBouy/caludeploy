94a1cb8e9798a18f287119a7a73f96580297a46e
JoakBouy <b.joak@alustudent.com>
2023-08-18 08:22:54 +0300

    update

--- clubs_and_events/urls.py
+++ clubs_and_events/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('api/community/', include('community.urls')),
     path('api/membership/', include('membership.urls')),
     path('api/user/', include('user.urls')),
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
